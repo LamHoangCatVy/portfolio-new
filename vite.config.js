959b1d7c8a5f174a8cedb7e35b48a3a50db3f1cf
Lam Hoang Cat Vy <catvyisstudying@gmail.com>
2025-05-15 23:31:45 +0700

    add config base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-  base: "/portfolio-2025/",
+  base: "/portfolio-new,
   plugins: [react(), tailwindcss()],
 });
